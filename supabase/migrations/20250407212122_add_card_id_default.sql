669bd5367c5c23ba1225355c6b521d2d197f472c
Tom Hendrickx <tom@provivo.be>
2025-04-27 11:31:02 +0200

    feat: Pull remote schema changes from dev

--- supabase/migrations/20250407212122_add_card_id_default.sql
+++
@@ -1,6 +0,0 @@
--- Ensure the required extension is enabled
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
--- Add the default value generator to the id column
-ALTER TABLE public.cards
-ALTER COLUMN id SET DEFAULT uuid_generate_v4();
